eeacc8adde4ffd0e914248b44b3bd9ddff000f4f
Code For New Life <codefornewlife@gmail.com>
2024-11-30 20:56:07 +0300

    F02

--- assets/js/boskery.js
+++ assets/js/boskery.js
@@ -923,3 +923,94 @@
     boskery_stretch();
   });
 })(jQuery);
+
+function magnify(imgID, zoom) {
+  var img, glass, w, h, bw;
+  img = document.getElementById(imgID);
+  
+  /*create magnifier glass:*/
+  glass = document.createElement("DIV");
+  glass.setAttribute("class", "img-magnifier-glass");
+  
+  /*insert magnifier glass:*/
+  img.parentElement.insertBefore(glass, img);
+  
+  /*set background properties for the magnifier glass:*/
+  glass.style.backgroundImage = "url('" + img.src + "')";
+  glass.style.backgroundRepeat = "no-repeat";
+  glass.style.backgroundSize = (img.width * zoom) + "px " + (img.height * zoom) + "px";
+  bw = 3;
+  w = glass.offsetWidth / 2;
+  h = glass.offsetHeight / 2;
+  
+  /*execute a function when someone moves the magnifier glass over the image:*/
+  glass.addEventListener("mousemove", moveMagnifier);
+  img.addEventListener("mousemove", moveMagnifier);
+  
+  /*and also for touch screens:*/
+  glass.addEventListener("touchmove", moveMagnifier);
+  img.addEventListener("touchmove", moveMagnifier);
+  
+  img.addEventListener("mouseenter", function() {
+    glass.style.display = "block"; // Büyüteci göster
+  });
+  
+  img.addEventListener("mouseleave", function() {
+    glass.style.display = "none"; // Büyüteci gizle
+  });
+
+  function moveMagnifier(e) {
+    var pos, x, y;
+    /*prevent any other actions that may occur when moving over the image*/
+    e.preventDefault();
+    
+    /*get the cursor's x and y positions:*/
+    pos = getCursorPos(e);
+    x = pos.x;
+    y = pos.y;
+    
+    /*prevent the magnifier glass from being positioned outside the image:*/
+    if (x > img.width - (w / zoom)) {x = img.width - (w / zoom);}
+    if (x < w / zoom) {x = w / zoom;}
+    if (y > img.height - (h / zoom)) {y = img.height - (h / zoom);}
+    if (y < h / zoom) {y = h / zoom;}
+    
+    /*set the position of the magnifier glass:*/
+    glass.style.left = (x - w) + "px"; // Büyüteci imlecin ortasında konumlandır
+    glass.style.top = (y - h) + "px";   // Büyüteci imlecin ortasında konumlandır
+    
+    /*display what the magnifier glass "sees":*/
+    glass.style.backgroundPosition = "-" + ((x * zoom) - w + bw) + "px -" + ((y * zoom) - h + bw) + "px";
+  }
+  
+  function getCursorPos(e) {
+    var a, x = 0, y = 0;
+    e = e || window.event;
+    
+    /*get the x and y positions of the image:*/
+    a = img.getBoundingClientRect();
+    
+    /*calculate the cursor's x and y coordinates, relative to the image:*/
+    x = e.pageX - a.left;
+    y = e.pageY - a.top;
+    
+    /*consider any page scrolling:*/
+    x = x - window.pageXOffset;
+    y = y - window.pageYOffset;
+    return {x : x, y : y};
+  }
+}
+
+/* Initiate Magnify Function with the id of the image, and the strength of the magnifier glass: */
+magnify("myimage1", 3);
+magnify("myimage2", 3);
+magnify("myimage3", 3);
+magnify("myimage4", 3);
+magnify("myimage5", 3);
+magnify("myimage6", 3);
+magnify("myimage7", 3);
+magnify("myimage8", 3);
+magnify("myimage9", 3);
+magnify("myimage10", 3);
+
+
